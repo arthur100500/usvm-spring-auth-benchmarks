c07cbff1d50fa14b6b0e6a37582cfd4efc364f49
Arthur Alekseev <arthur100500@gmail.com>
2025-02-15 14:56:01 +0300

    Remove csrf
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- src/main/java/org/usvm/spring/auth/benchmarks/config/SecurityConfig.java
+++ src/main/java/org/usvm/spring/auth/benchmarks/config/SecurityConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -27,6 +29,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 );
 
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
@@ -56,5 +59,4 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-
 }
